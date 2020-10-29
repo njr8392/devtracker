c7bcf917fd67b6843886d854ac460c5f407712f0
Nicholas Rodgers <nrodgers88@gmail.com>
2020-10-28 23:01:01 -0400

    added timer

--- main.go
+++ main.go
@@ -13,19 +13,23 @@ func main() {
 
 	var (
 		//	buf       bytes.Buffer
+		started   bool
+		start     time.Time
 		ipaddress string = os.Getenv("IP")
+		device    string = os.Getenv("DEVICE")
 		//switch logger from buffer to os.Stdout
-		logger = log.New(os.Stdout, "Dante's Phone: ", log.Ltime)
+		logger = log.New(os.Stdout, device, log.Ltime)
 	)
+	//	f, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 
-	f, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
+	//	if err != nil {
+	//		log.Fatalf("error opening file: %v", err)
+	//	}
 
-	logger.SetOutput(f)
-	cmd := exec.Command("ping", "-i 600", ipaddress)
+	//	defer f.Close()
+	//	logger.SetOutput(f)
+
+	cmd := exec.Command("ping", "-i 10", ipaddress)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
@@ -38,14 +42,31 @@ func main() {
 	r := bufio.NewReader(stdout)
 	for {
 		line, err := r.ReadBytes('\n')
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
-		time.Sleep(time.Second)
-		if strings.Contains(string(line), "Destination Host Unreachable") {
+
+		if isDisconnected(string(line)) && !started {
 			logger.Printf("Disconnected")
+			started = true
+			start = time.Now()
+
+		} else if !isDisconnected(string(line)) && started {
+			end := time.Now()
+			_, month, day := end.Date()
+			elapsed := end.Sub(start)
+			logger.Printf("%s was disconnected from the wifi for %s on %s %s", device, elapsed, month, day)
+			started = false
+
 		} else {
-				logger.Printf("Connected")
+			logger.Printf("Connected")
 		}
 	}
+}
+
+func isDisconnected(output string) bool {
+	if strings.Contains(output, "Destination Host Unreachable") {
+		return true
+	}
+	return false
 }
