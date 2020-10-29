0fda9a1f9884f909bb47e02dbeffdb881d3afd2c
Nicholas Rodgers <nrodgers88@gmail.com>
2020-10-29 01:14:59 -0400

    fixed log formatter

--- main.go
+++ main.go
@@ -20,14 +20,14 @@ func main() {
 		//switch logger from buffer to os.Stdout
 		logger = log.New(os.Stdout, device, log.Ltime)
 	)
-	//	f, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		f, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 
-	//	if err != nil {
-	//		log.Fatalf("error opening file: %v", err)
-	//	}
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
 
-	//	defer f.Close()
-	//	logger.SetOutput(f)
+		defer f.Close()
+		logger.SetOutput(f)
 
 	cmd := exec.Command("ping", "-i 10", ipaddress)
 	stdout, err := cmd.StdoutPipe()
@@ -41,13 +41,13 @@ func main() {
 
 	r := bufio.NewReader(stdout)
 	for {
+
 		line, err := r.ReadBytes('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		if isDisconnected(string(line)) && !started {
-			logger.Printf("Disconnected")
 			started = true
 			start = time.Now()
 
@@ -55,11 +55,9 @@ func main() {
 			end := time.Now()
 			_, month, day := end.Date()
 			elapsed := end.Sub(start)
-			logger.Printf("%s was disconnected from the wifi for %s on %s %d", device, elapsed, month, day)
+			logger.Printf("was disconnected from the wifi for %v from %s to %s", elapsed.Round(time.Second), start.Format(time.Stamp), end.Format(time.Stamp))
 			started = false
 
-		} else {
-			logger.Printf("Connected")
 		}
 	}
 }
